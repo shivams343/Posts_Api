14a77a3ed81cd1c97ef2b5f8849b5fe473202538
shivams343 <shivamsharmaemail@gmail.com>
2020-10-30 21:17:58 +0530

    Updates

---
+++ precache-manifest.662b6e64c24f22b3a82e4c6b2a9116cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c4c4710f75dde770671cadc9d4278f",
+    "url": "/Posts_Api/index.html"
+  },
+  {
+    "revision": "29f0f9b362f46a3e87a0",
+    "url": "/Posts_Api/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5f3c42a4584fd0c96cd6",
+    "url": "/Posts_Api/static/js/2.457afe54.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Posts_Api/static/js/2.457afe54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f0f9b362f46a3e87a0",
+    "url": "/Posts_Api/static/js/main.e13246c0.chunk.js"
+  },
+  {
+    "revision": "df59d1d0b087d3b7adf2",
+    "url": "/Posts_Api/static/js/runtime-main.e65e29cc.js"
+  }
+]);
